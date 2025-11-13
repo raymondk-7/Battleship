eb046329e0ea013134efa89aa1f052345d89c314
raymond.k <144247568+raymondk-7@users.noreply.github.com>
2025-11-13 02:15:07 +0000

    Add files via upload

---
+++ main/src/BattleshipGame.java
@@ -0,0 +1,16 @@
+public class BattleshipGame {
+
+    // Constant values - used instead of final variables
+    public enum cellState {
+        WATER, // Untouched water
+        SHIP, // Part of a placed ship
+        HIT, // When a ship part has been hit
+        MISS, // A missed shot; water that was hit
+    }
+
+    public static void main(String[] a) {
+
+    }
+
+
+}
